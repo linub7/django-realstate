f2dbef82884da8e78602f1d783c8f0061bca85d9
a1a2a3a4a5 <linub7@gmail.com>
2023-03-30 20:25:04 +0430

    pages templates - base layout

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') # root url = ''
+    path('', views.index, name='index'), # root url = ''
+    path('about', views.about, name='about')
 ]
